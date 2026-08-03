a56392fbe15916099989b4bc89b3c85870762d4a
pop372440-art <pop372440@gmail.com>
2026-08-03 11:35:29 +0700

    Update manifest.ts

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,7 +11,7 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#0b132b',
     icons: [
       {
-        src: '/apple-icon.png',
+        src: '/icon1.png',
         sizes: 'any',
         type: 'image/png',
       },
