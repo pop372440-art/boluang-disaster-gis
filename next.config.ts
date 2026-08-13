5bc08b1244a1a6c72bca4a7e0956625c4e1445d5
pop372440-art <pop372440@gmail.com>
2026-08-13 09:53:40 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/onwr/:path*',
+        destination: 'https://api-v3.thaiwater.net/api/v1/thaiwater30/public/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
