7461fe5c455ade21aa81b1d2b04cbc28bdcee073
pop372440-art <pop372440@gmail.com>
2026-08-16 13:34:58 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+// 🚨 1. Import ผู้ช่วยบ่อหลวงเข้ามาใช้งาน
+import SmartHelper from '@/components/SmartHelper';
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -27,7 +30,15 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        
+        {/* เนื้อหาหลักของเว็บ */}
+        {children}
+
+        {/* 🚨 2. วางแชทบอทไว้ก่อนปิด Body (ตัวบอทจะลอยอยู่มุมขวาล่างเสมอ) */}
+        <SmartHelper />
+        
+      </body>
     </html>
   );
 }
