0f5b1baf6f2823ce0cfae16c8e5d498404d95777
pop372440-art <pop372440@gmail.com>
2026-08-18 16:32:13 +0700

    Update manifest.ts

--- app/manifest.ts
+++ app/manifest.ts
@@ -2,19 +2,24 @@ import { MetadataRoute } from 'next'
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: 'Bo Luang GIS | ระบบจัดการสาธารณภัย',
-    short_name: 'BL GIS',
-    description: 'ระบบสารสนเทศภูมิศาสตร์ ต.บ่อหลวง',
+    name: 'ระบบภูมิสารสนเทศ เทศบาลตำบลบ่อหลวง',
+    short_name: 'บ่อหลวง GIS',
+    description: 'ระบบสารสนเทศทางภูมิศาสตร์เพื่อบริหารจัดการด้านสาธารณภัย เทศบาลตำบลบ่อหลวง',
     start_url: '/',
     display: 'standalone',
     background_color: '#0b132b',
-    theme_color: '#0b132b',
+    theme_color: '#38bdf8',
     icons: [
       {
-        src: '/apple-icon.png',
-        sizes: 'any',
+        src: '/Logo.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/Logo.png',
+        sizes: '512x512',
         type: 'image/png',
       },
     ],
   }
-} 
+}
