d6c14849d1de1bae9be9d6ce2bf8c7180df5c7e7
pop372440-art <pop372440@gmail.com>
2026-08-29 15:03:31 +0700

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: ['/admin/', '/api/'], // ซ่อนหน้า API และหน้า Admin จาก Google
+      disallow: ['/admin', '/api/'], // ซ่อนหน้า API และหน้า Admin จาก Google
     },
     sitemap: 'https://boluang-disaster-gis.vercel.app/sitemap.xml',
   }
